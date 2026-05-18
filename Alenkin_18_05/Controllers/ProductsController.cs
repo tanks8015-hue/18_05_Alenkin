2230d7446471feb68c5f4d15263604c7a7cec401
record1321-hash <record1321@gmail.com>
2026-05-18 12:46:13 +0400

    добавил продукты в API

---
+++ Alenkin_18_05/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Alenkin_18_05.Data;
+using Alenkin_18_05.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace Alenkin_18_05.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _context.Products.Include(p => p.Category).ToListAsync();
+            return Ok(products);
+        }
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            if (product == null)
+            {
+                return BadRequest("Данные товара не переданы"); 
+            }
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProducts), new { id = product.ProductId }, product);
+        }
+    }
+}

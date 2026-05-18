5792a0d701330e8c90306d5aa430cf6f1df17d48
record1321-hash <record1321@gmail.com>
2026-05-18 12:44:18 +0400

    добавил категории API Controller

---
+++ Alenkin_18_05/Controllers/CategoriesController.cs
@@ -0,0 +1,45 @@
+﻿using Alenkin_18_05.Data;
+using Alenkin_18_05.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace Alenkin_18_05.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+            try
+            {
+                var categories = await _context.Categories.ToListAsync();
+                return Ok(categories); // Статус 200 OK
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Внутренняя ошибка сервера"); // Статус 500
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetCategory(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+    }
+}
